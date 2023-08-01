8b277c745b17dc67e233dc3c85f54b078345b79d
oliver <oliverlenton@me.com>
2023-08-01 22:29:40 +0100

    Cleanup old npm builds

--- dist/ValidateFields.d.ts
+++
@@ -1,7 +0,0 @@
-import { InputData } from "./InputData";
-/**
- * Interface for fields to pass to validate
- */
-export interface ValidateFields<Datatype> {
-    [key: string]: InputData<Datatype>;
-}
--- dist/validate.d.ts
+++
@@ -1,7 +0,0 @@
-import { ValidateFields } from "./ValidateFields";
-/**
- *
- * @param fields
- * @returns
- */
-export declare const validate: <Datatype>(fields: ValidateFields<Datatype>) => boolean;
