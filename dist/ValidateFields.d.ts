bb2af877eec2f54c1793e7df0ac61743d6736a1a
oliver <oliverlenton@me.com>
2023-08-01 22:22:08 +0100

    Add build comment

--- dist/ValidateFields.d.ts
+++ dist/ValidateFields.d.ts
@@ -1,4 +1,7 @@
 import { InputData } from "./InputData";
+/**
+ * Interface for fields to pass to validate
+ */
 export interface ValidateFields<Datatype> {
     [key: string]: InputData<Datatype>;
 }
